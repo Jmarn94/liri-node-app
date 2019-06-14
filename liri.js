7a8e3de7511fc0e7f189380a1effe3ed00f8427e
Jason Arnold <jmarn94@gmail.com>
2019-06-14 16:55:26 -0500

    updated code to work

--- liri.js
+++ liri.js
@@ -1,110 +1,106 @@
-//require("dotenv").config();
-//var moment = require('moment');
-var Spotify = require('node-spotify-api');
-var fs = require("fs");
-var axios = require("axios");
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
-var input = "";
-var nodeArgs = process.argv;
-var action = process.argv[2];
+require("dotenv").config();
+var request = require("request");
+var keys = require('./keys.js')
+var Spotify = require("node-spotify-api");
+var moment = require("moment");
 
-switch(action){
-    case "concert-this":
-    concert();
-    break;
+var userInput = process.argv[2];
 
-    case "spotify-this-song":
-    spotify();
-    break;
-
-    case "movie-this":
-    movie();
-    break;
-
-    case "do-what-it-says":
-    whatever();
-    break;
-
-    default:
-    console.log("After 'node liri.js' Please enter one of the following Commands: ");
-    console.log("*movie");
-    console.log("*concert");
-    console.log("*spotify");
-    console.log("*do-what-it-says")
-    break;
-};
-
-
-function concert(){
-    var queryURL = "https://rest.bandsintown.com/artists/" +
-    input +
-    "/events?app_id=codingbootcamp+limit=3";
-    axios.get(queryUrl).then(function(response) {
-        for (var i = 0; i < response.data.length; i++) {
-          console.log("==============================================");
-          console.log("");
-          console.log("- Name Of The Venue: " + response.data[i].venue.name);
-          console.log("- Location Of The Venue: " + response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country);
-          console.log("- Date Of The Event: " + moment(response.data[i].datetime).toDate());
-          console.log("");
-          console.log("==============================================");
+function movieThis() {
+    var thisMovie = process.argv[3]
+    var queryUrl = "http://www.omdbapi.com/?t=" + thisMovie + "&y=&plot=short&apikey=trilogy";
+    request(queryUrl, (err, response, body) => {
+        if (err) {
+            return err;
         }
-      })
-      .catch(function(err) {
-        console.log(err);
-      });
-    }
+        if (response.statusCode === 200) {
+            let json = JSON.parse(body);
 
-    
-function spotify(){
-    var songName = ""
-    for (var i = 3; i <nodeArgs.length; i++){
-        if(i > 3 && i <nodeArgs.length) {
-            songName + "+" + nodeArgs[i]; 
+            console.log(("Title: ") + json.Title);
+            console.log(("Plot: ") + json.Plot);
+            console.log(("Actors: ") + json.Actors);
+            console.log(("Year Released: ") + json.Released);
+            console.log(("IMDB Ratings: ") + json.imdbRating);
+            console.log(("Country: ") + json.Country);
+            console.log(("Language: ") + json.Language);
         }
-        else {
-            songName += nodeArgs[i];
-        }
-    }
-    if (songName === "") {
-        songName= "Live Wire";
-    }
-};
-
-
-function movie() {
-    var movieName = "";
-    for (var i = 3; i <nodeArgs.length; i++){
-        if (i > 3 && i < nodeArgs.length) {
-            movieName = movieName + "+" + nodeArgs[i];
-        }
-        else {
-            movieName += nodeArgs[i];
-        }
-    }
-    if (movieName === "") {
-        movieName= "Mr. Nobody";
-    };
-    var queryURL = "http://www.omdbapi.com/?i=tt3896198&apikey=7216f2f9" + movieName + "&y=&plot=short&apikey=trilogy";
-
-    axios.get(queryURL).then(
-        function(response) {
-            console.log("Movie Title: " + response.data.Title);
-            console.log("Release Yeare: " + response.data.Year);
-            console.log("IMDB Rating: " + response.data.imdbrating);
-            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[2].Value);
-            console.log("Movie Plot: " + response.data.Plot)
-            console.log("Actors: " + response.data.Actors)
-        }
-    );
-};
-function whatever() {
-    fs.readFile("random.txt", "utf8", function(err, data) {
-      if (err) return console.log(err);
-      console.log(data);
-      input = data;
-      spotifyThis();
     })
-  }
-  
+}
+
+
+function concertThis() {
+    var artist = process.argv[3]
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    request(queryUrl, (err, response, body) => {
+        if (err) {
+            return err;
+        }
+        if (response.statusCode === 200) {
+            let json = JSON.parse(body);
+            for (var i = 0; i < 5; i++) {
+
+                console.log(("Venue: ") + json[i].venue.name);
+                console.log(("Location: ") + json[i].venue.city);
+                console.log((moment(json[i].datetime).format("MM/DD/YY")));   
+
+            }
+        }
+    })
+}
+
+function spotifyThis() {
+    var spotify = new Spotify(keys.spotify);
+    var songName = process.argv[3];
+
+    spotify.search({
+        type: "track",
+        query: songName,
+    }, function (err, data) {
+        if (err) {
+            return err;
+        }
+
+        else {
+            console.log(("Artist: ") + data.tracks.items[0].artists[0].name);
+            console.log(("Song: ") + data.tracks.items[0].name);
+            console.log(("Album: ") + data.tracks.items[0].album.name);
+            console.log(("Preview: ") + data.tracks.items[0].preview_url);
+        }
+    })
+}
+
+
+function doThis() {
+    fs.readFile("random.txt", "utf8", (err, data) => {
+        if (err) {
+            return console.log(error);
+        } else {
+
+        }
+
+    })
+
+}
+
+
+///
+switch (userInput) {
+
+    case 'movie-this':
+        movieThis();
+        break;
+
+    case 'concert-this':
+        concertThis();
+        break;
+
+        case 'spotify-this-song':
+        spotifyThis();
+        break;
+
+    case 'do-what-it-says':
+        doThis();
+        break;
+}
+
+
